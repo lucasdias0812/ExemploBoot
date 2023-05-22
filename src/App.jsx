4e2541d90580a2dd394fe7886b58e4102c5a67b5
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2023-05-22 10:03:51 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ColorSchemesExample from './ColorSchemesExample'
+import CarouselTeste from './CarouselTeste'
+
+
+function App() {
+  return(
+    <div>
+ <ColorSchemesExample/>
+ <CarouselTeste/>
+    </div>
+  )
+}
+
+export default App
